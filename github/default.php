b540ac56e9b3a2306901c9f937f86a21cbd3e9a5
jayrpatron <64820045+jayrpatron@users.noreply.github.com>
2023-11-08 23:39:31 +0800

    push

---
+++ github/default.php
@@ -0,0 +1,53 @@
+<html lang="en">
+    <head>
+        <title>Built With Bard</title>
+        <link rel="stylesheet" href="styles.css">
+        <meta charset="utf-8">
+        <meta content="IE=edge,chrome=1" http-equiv="X-UA-Compatible">
+        <meta content="Default page" name="description">
+        <meta content="width=device-width, initial-scale=1" name="viewport">
+        <meta name="description" content="Built with Bard is a personal website project continuously developed with help from generative AI.">
+        <link rel="preconnect" href="https://fonts.googleapis.com">
+        <link rel="canonical" href="https://builtwithbard.com">
+    </head>
+<!-- Google tag (gtag.js) -->
+<script async src="https://www.googletagmanager.com/gtag/js?id=G-263N4KM2QW"></script>
+<script>
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
+
+  gtag('config', 'G-263N4KM2QW');
+</script>
+<body>
+<!-- Google Tag Manager (noscript) -->
+<noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-K2ZJXL8B"
+height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
+<!-- End Google Tag Manager (noscript) -->
+<header>
+  <h1>Built With Bard</h1>
+    <div class="header-links">
+      <a href="/">Home</a>
+      <a href="blog.html">Blog</a>
+      <a href="about">About</a>
+      <a href="faqs">FAQs</a>
+      <a href="projects.html">Projects</a>
+    </div>
+</header>
+  <main>
+    <section>
+    <figure>
+    <img src="assets/fotor-ai-20230814113813_720px.jpg" alt="AI generated image of a man creating a website in a dark room with the help of generative AI">
+      <figcaption>AI generated image of a man creating a website in a dark room with the help of generative AI. Image generated via <a href="https://www.fotor.com/" target="_blank">Fotor</a>.</figcaption>
+    </figure>
+      <p class="blog-post">This is Built With Bard, a website that I, having little to no experience in web development, continuously develop with (the help of) generative AI. There is no particular purpose for this website (yet) other than documenting the whole development process. I am hoping that someday it may inspire others to "create with impact" with the help of generative AI.<br><br>If you're new to this site, please do visit the <a href="/blog.html">Blog</a> section where I place the updates.<br><br>They used to be on the homepage but I decided to subsume them within a new subpage so that the landing page becomes more flexible.<br><br>Please do visit the <a href="about">About</a> and <a href="faqs">FAQs</a> page as well, which I will also continuously develop.</p><br><br>
+      <iframe src="https://embeds.beehiiv.com/051727eb-88aa-4a1f-8256-a129990669dc" data-test-id="beehiiv-embed" width="100%" height="320" frameborder="0" scrolling="no" style="border-radius: 4px; border: 2px solid #e5e7eb; margin: 0; background-color: transparent;"></iframe>
+    </section>
+  </main>
+<footer>
+    <p>Copyright &copy; 2023</p>
+  </footer>
+<!-- Beehiiv attribution tag -->
+<script type="text/javascript" async src="https://embeds.beehiiv.com/attribution.js"></script>
+</body>
+</html>
